1f444a11e1a278c5b2fb7361d1e8b2f1cd81f4c0
Marco Elste <marco.elste@web.de>
2025-07-03 19:00:23 +0200

    feat: Updated app.js: archiveNote()

--- static/app.js
+++ static/app.js
@@ -1,6 +1,7 @@
 import { getSingleNote } from "./templates.js";
 
 let notes = [];
+let archive = [];
 
 function loadStorage() {
     const storeNotes = JSON.parse(localStorage.getItem('notes'));
@@ -9,15 +10,20 @@ function loadStorage() {
 
 function saveStorage() {
     localStorage.setItem('notes', JSON.stringify(notes));
+    localStorage.setItem('archive', JSON.stringify(archive));
 }
 
 function getUserInput() {
     const refInput = document.querySelectorAll('input[type=text]');
-    notes.push({
-        noteName: refInput[0].value,
-        noteContent: refInput[1].value
-    });
-    saveStorage();
+    
+    if (refInput[0].value.length > 0 && refInput[1].value.length > 0) {
+        notes.push({
+            noteName: refInput[0].value,
+            noteContent: refInput[1].value
+        });
+        saveStorage();
+        renderNotes();
+    }
 }
 
 function init() {
@@ -37,6 +43,17 @@ function renderNotes() {
     notes.forEach(note => {
         refUls[0].innerHTML += getSingleNote(note.noteName, note.noteContent);
     })
+    document.querySelectorAll('.saved-notes').forEach((saveNote, noteIndex) => {
+        saveNote.addEventListener('click', () => {
+            archiveNote(noteIndex);
+        });
+    });
+}
+
+function archiveNote(noteIndex) {
+    const archievedNotes = notes.splice(noteIndex, 1);
+    archive.push(archievedNotes[0]);
+    saveStorage();
 }
 
 init();
