6b5b905f43b074b6827afaf6ecc631dd77adaed1
Marco Elste <marco.elste@web.de>
2025-07-04 10:30:52 +0200

    feat: Updated app.js: trashNote()

--- static/app.js
+++ static/app.js
@@ -1,19 +1,24 @@
-import { getSingleNote, getSingleArchiveNote } from "./templates.js";
+import { getSingleNote, getSingleArchiveNote, getSingleTrashNote } from "./templates.js";
 
 let notes = [];
 let archive = [];
+let trashes = [];
 
 // #region Mangement functions
 function loadStorage() {
     const storeNotes = JSON.parse(localStorage.getItem('notes'));
     const archievedNotes = JSON.parse(localStorage.getItem('archive'));
+    const trashedNotes = JSON.parse(localStorage.getItem('trashes'));
+
     if (storeNotes != null) notes = storeNotes;
     if (archievedNotes != null) archive = archievedNotes;
+    if (trashedNotes != null) trashes = trashedNotes;
 }
 
 function saveStorage() {
     localStorage.setItem('notes', JSON.stringify(notes));
     localStorage.setItem('archive', JSON.stringify(archive));
+    localStorage.setItem('trashes', JSON.stringify(trashes));
 }
 
 function getUserInput() {
@@ -43,20 +48,38 @@ function renderNotes() {
     refUls.forEach(ul => {
         ul.innerHTML = '';
     });
+    // Notes
     notes.forEach(note => {
         refUls[0].innerHTML += getSingleNote(note.noteName, note.noteContent);
     });
+    //Archived Notes
     archive.forEach (note => {
         refUls[1].innerHTML += getSingleArchiveNote(note.noteName, note.noteContent);
     });
+    //Trahed Notes
+    trashes.forEach (note => {
+        refUls[2].innerHTML += getSingleTrashNote(note.noteName, note.noteContent);
+    });
+    addAllEventlisteners();
+}
+
+function addAllEventlisteners() {
+    // Archive noraml Note
     document.querySelectorAll('.saved-notes').forEach((saveNote, noteIndex) => {
         saveNote.addEventListener('click', () => {
             archiveNote(noteIndex);
         });
     });
+    // unarchive Note
     document.querySelectorAll('.unarchive-notes').forEach((note, noteIndex) => {
         note.addEventListener('click', () => {
             unarchiveNote(noteIndex);
+        });
+    });
+    // trash noraal Note
+    document.querySelectorAll('.note-del').forEach((note, noteIndex) => {
+        note.addEventListener('click', () => {
+            trashNote(noteIndex);
         });
     });
 }
@@ -72,6 +95,13 @@ function archiveNote(noteIndex) {
 function unarchiveNote(noteIndex) {
     const unarchiedNotes = archive.splice(noteIndex, 1);
     notes.push(unarchiedNotes[0]);
+    saveStorage();
+    renderNotes();
+}
+
+function trashNote(noteIndex) {
+    const trashedNotes = notes.splice(noteIndex, 1);
+    trashes.push(trashedNotes[0]);
     saveStorage();
     renderNotes();
 }
--- static/templates.js
+++ static/templates.js
@@ -1,11 +1,28 @@
 export function getSingleNote(name, content) {
     return /*html*/`
-        <li>${name} - ${content}<button class="notes-btn saved-notes"><img src="assets/icons/save_blue.svg"></button></li>
+        <li>
+            ${name} - ${content}
+            <button class="notes-btn saved-notes"><img src="assets/icons/save_blue.svg"></button>
+            <button class="notes-btn trashed-notes note-del"><img src="assets/icons/trash_blue.svg"></button>
+        </li>
     `
 }
 
 export function getSingleArchiveNote(name, content) {
     return /*html*/`
-        <li>${name} - ${content}<button class="notes-btn unarchive-notes"><img src="assets/icons/uparrow_blue.svg"></button></li>
+        <li>
+            ${name} - ${content}
+            <button class="notes-btn unarchive-notes"><img src="assets/icons/uparrow_blue.svg"></button>
+            <button class="notes-btn trashed-notes archive-del"><img src="assets/icons/trash_blue.svg"></button>
+        </li>
+    `
+}
+
+export function getSingleTrashNote(name, content) {
+    return /*html*/`
+        <li>
+            ${name} - ${content}
+            <button class="notes-btn trashed-notes del-del"><img src="assets/icons/trash_blue.svg"></button>
+        </li>
     `
 }
